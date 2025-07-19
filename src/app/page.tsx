dcc66fea8a8a997a1fa82eecf78d186812a699f5
Nanphipat Wongted <nanphipat.wongted@icloud.com>
2025-07-19 22:20:45 +0700

    design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,15 +18,6 @@ type ContactItemProps = {
 const ContactItem = ({ icon, text }: ContactItemProps) => {
   return (
     <div className="flex items-center space-x-4">
-      <div className="text-teal-600 bg-amber-100 p-2 rounded-full">{icon}</div>
-      <div>{text}</div>
-    </div>
-  );
-};
-
-const ContactItemMobile = ({ icon, text }: ContactItemProps) => {
-  return (
-    <div className="flex items-center space-x-4">
       <div className="text-white bg-gray-700 p-2 rounded-full">{icon}</div>
       <div>{text}</div>
     </div>
@@ -54,84 +45,89 @@ export default function Home() {
             priority
           />
         </div>
-        <div className="w-2/3 bg-amber-100 flex flex-col relative">
-          <AnimatePresence>
-            {!showContact ? (
-              <button
-                className="absolute top-4 right-4 border text-amber-100 bg-teal-700 rounded-md hover:text-white cursor-pointer px-4"
-                onClick={() => setShowContact(true)}
-              >
-                Contact
-              </button>
-            ) : (
-              <motion.div
-                key="contact-box"
-                initial={{ opacity: 0, x: 100, y: 0 }}
-                animate={{ opacity: 1, x: 0, y: 0 }}
-                exit={{ opacity: 0, x: 100, y: 0 }}
-                transition={{ duration: 0.2 }}
-                className="absolute top-4 right-4 rounded-md p-4 shadow-lg w-80 text-amber-100 bg-teal-700 flex flex-col justify-between"
-              >
-                <div className="flex flex-col space-y-4">
-                  <ContactItem
-                    icon={<PhoneIcon className="w-6 h-6" />}
-                    text="0926258828"
-                  />
-                  <ContactItem
-                    icon={<EnvelopeIcon className="w-6 h-6" />}
-                    text="nanphipat.wongted@icloud.com"
-                  />
-                  <ContactItem
-                    icon={<MapPinIcon className="w-6 h-6" />}
-                    text="Samutprakarn Thailand"
-                  />
-                </div>
-                <div className="flex justify-center mt-4">
-                  <button
-                    className="bg-amber-100 text-teal-700 font-semibold px-4 py-1 rounded hover:bg-amber-200"
-                    onClick={() => setShowContact(false)}
-                  >
-                    Close
-                  </button>
-                </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
+        <div className="w-2/3 flex flex-col relative overflow-hidden">
+          <div
+            className="absolute inset-0 bg-cover bg-right filter blur-sm scale-100 pointer-events-none"
+            style={{ backgroundImage: "url('/profile.jpeg')" }}
+          ></div>
 
-          <div className="flex flex-col space-y-4 p-20 flex-1 justify-center">
-            <div className="text-8xl font-bold text-teal-700">
+          <div className="relative flex flex-col space-y-4 p-20 flex-1 justify-center text-white z-10">
+            <AnimatePresence>
+              {!showContact ? (
+                <button
+                  className="absolute top-4 right-4 border text-white rounded-md hover:text-black cursor-pointer px-4"
+                  onClick={() => setShowContact(true)}
+                >
+                  Contact
+                </button>
+              ) : (
+                <motion.div
+                  key="contact-box"
+                  initial={{ opacity: 0, x: 100, y: 0 }}
+                  animate={{ opacity: 1, x: 0, y: 0 }}
+                  exit={{ opacity: 0, x: 100, y: 0 }}
+                  transition={{ duration: 0.2 }}
+                  className="absolute top-4 right-4 rounded-md p-4 shadow-lg w-80 text-black bg-white flex flex-col justify-between z-20"
+                >
+                  <div className="flex flex-col space-y-4">
+                    <ContactItem
+                      icon={<PhoneIcon className="w-6 h-6" />}
+                      text="0926258828"
+                    />
+                    <ContactItem
+                      icon={<EnvelopeIcon className="w-6 h-6" />}
+                      text="nanphipat.wongted@icloud.com"
+                    />
+                    <ContactItem
+                      icon={<MapPinIcon className="w-6 h-6" />}
+                      text="Samutprakarn Thailand"
+                    />
+                  </div>
+                  <div className="flex justify-center mt-4">
+                    <button
+                      className="border border-black text-black hover:text-gray-700 hover:border-gray-700 font-semibold px-4 py-1 rounded cursor-pointer"
+                      onClick={() => setShowContact(false)}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+
+            <div className="text-8xl font-bold text-white">
               NANPHIPAT WONGTED
             </div>
-            <div className="h-px bg-teal-700 my-4"></div>
+            <div className="h-px bg-white my-4"></div>
 
-            <div className="text-2xl text-right font-bold text-teal-700">
+            <div className="text-2xl text-right font-bold text-white">
               Frontend Developer
             </div>
 
-            <div className="text-lg font-light text-white indent-8 bg-teal-700 p-4 rounded-2xl">
+            <div className="text-lg font-light indent-8 bg-white text-black p-4 rounded-2xl">
               {seeMore ? fullText : shortText}
               <button
                 onClick={() => setSeeMore(!seeMore)}
-                className="ml-2 text-sm underline text-white hover:text-amber-200"
+                className="ml-2 text-sm underline text-black hover:text-gray-700"
               >
                 {seeMore ? "Show Less" : "Read More"}
               </button>
             </div>
           </div>
 
-          <div className="flex h-20">
-            <div className="flex-1 flex items-center justify-center text-xl text-teal-700">
-              <button className="border border-teal-700 w-1/2 h-1/2 rounded-md hover:text-white hover:bg-teal-700 cursor-pointer">
+          <div className="flex h-20 absolute bottom-0 w-full z-10">
+            <div className="flex-1 flex items-center justify-center text-xl text-white">
+              <button className="border border-white w-1/2 h-1/2 rounded-md hover:text-black hover:bg-white cursor-pointer">
                 Profile
               </button>
             </div>
-            <div className="flex-1 flex items-center justify-center text-xl text-teal-700">
-              <button className="border border-teal-700 w-1/2 h-1/2 rounded-md hover:text-white hover:bg-teal-700 cursor-pointer">
+            <div className="flex-1 flex items-center justify-center text-xl text-white">
+              <button className="border border-white w-1/2 h-1/2 rounded-md hover:text-black hover:bg-white cursor-pointer">
                 Education
               </button>
             </div>
-            <div className="flex-1 flex items-center justify-center text-xl text-teal-700">
-              <button className="border border-teal-700 w-1/2 h-1/2 rounded-md hover:text-white hover:bg-teal-700 cursor-pointer">
+            <div className="flex-1 flex items-center justify-center text-xl text-white">
+              <button className="border border-white w-1/2 h-1/2 rounded-md hover:text-black hover:bg-white cursor-pointer">
                 Work Experience
               </button>
             </div>
@@ -168,15 +164,15 @@ export default function Home() {
                   className="absolute top-4 right-4 rounded-md p-4 shadow-lg w-65 text-black bg-white flex flex-col justify-between text-xs"
                 >
                   <div className="flex flex-col space-y-4">
-                    <ContactItemMobile
+                    <ContactItem
                       icon={<PhoneIcon className="w-6 h-6" />}
                       text="0926258828"
                     />
-                    <ContactItemMobile
+                    <ContactItem
                       icon={<EnvelopeIcon className="w-6 h-6" />}
                       text="nanphipat.wongted@icloud.com"
                     />
-                    <ContactItemMobile
+                    <ContactItem
                       icon={<MapPinIcon className="w-6 h-6" />}
                       text="Samutprakarn Thailand"
                     />
